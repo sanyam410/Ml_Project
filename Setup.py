abf778910a0c385d592a2335071c2e7b960c0acb
Sanyam <sanyamjha420@gmail.com>
2026-09-19 18:37:55 +0530

    Second commit

---
+++ Setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+from typing import List
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+    
+    return requirements
+setup(
+    name="Ml_Project",version="0.0.1",
+    author="Sanyam",
+    author_email="sanyamjha420@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("Requirements.txt")
+    )
