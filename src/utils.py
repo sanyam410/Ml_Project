b400467e39bfe91ee3b8aca8bee52caaaa38df28
Sanyam <sanyamjha420@gmail.com>
2026-09-21 15:15:17 +0530

    Prediction Pipeline and App

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 import os
 import sys
-
+import pickle
 import numpy as np
 import pandas as pd
 import dill
@@ -38,4 +38,11 @@ def evaluate_models(x_train, y_train, x_test, y_test, models):
         return report
     except Exception as e:
         raise CustomException(e, sys) from e  # type: ignore[arg-type]
-    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys) #type: ignore
